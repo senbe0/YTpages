85219d887bca8e4f1f700b685508b4df82bf4366
EijiSambei <sambei13579@gmail.com>
2023-04-23 15:47:23 +0900

    modify port number

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,7 +1,7 @@
 
 
 export default async function handler(req, res) {
-  let response = await fetch("http://localhost:8005/getVideoObjList");
+  let response = await fetch("http://localhost:8100/getVideoObjList");
   response = await response.json();
   let video_obj_list = response.video_obj_list;
 
