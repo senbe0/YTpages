613eca03832b11cdc1273255ac3165e775e4a8ea
EijiSambei <sambei13579@gmail.com>
2023-04-30 16:59:10 +0900

    add analytics into HEAD

--- components/Head.js
+++ components/Head.js
@@ -5,8 +5,23 @@ const Head_site = () => {
     return (
         <div>
             <Head>
-            <title>Hololive同時接続者数 リアルタイム シンプルUI</title>
-            <meta name="description" content="My page description" />
+                <title>Hololive同時接続者数 リアルタイム シンプルUI</title>
+                <meta name="description" content="Holiveの同時接続者数ランキングをチェックする。シンプルなUIで、勢いのあるライブが一目で分かる。" />
+                {/* Google Analytics */}
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-G6BYLQRKYW"
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-G6BYLQRKYW');
+                `,
+                }}
+                />
             </Head>
         </div>
     )
