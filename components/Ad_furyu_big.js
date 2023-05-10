cb80827cdbe11e2337776f2a677e58e92533b619
EijiSambei <sambei13579@gmail.com>
2023-05-10 16:05:06 +0900

    change ad

--- components/Ad_furyu_big.js
+++ components/Ad_furyu_big.js
@@ -3,9 +3,9 @@
 const Ad_furyu_big = () => {
     return (
         <div>
-            <a href="https://px.a8.net/svt/ejp?a8mat=3T6UM5+B1459U+3X2W+60OXD" rel="nofollow">
-            <img border="0" width="250" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=230427005667&wid=001&eno=01&mid=s00000018284001011000&mc=1"></img></a>
-            <img border="0" width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=3T6UM5+B1459U+3X2W+60OXD" alt=""></img>
+            <a href="https://px.a8.net/svt/ejp?a8mat=3T6UM4+6VOCFM+50+2HMT4H" rel="nofollow">
+            <img border="0" width="250" height="250" alt="" src="https://www26.a8.net/svt/bgt?aid=230427004416&wid=001&eno=01&mid=s00000000018015055000&mc=1"></img></a>
+            <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3T6UM4+6VOCFM+50+2HMT4H" alt=""></img>
         </div>
     )
 }
