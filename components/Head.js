6183706cd976e3263711413fadb464b7de3b46d4
EijiSambei <sambei13579@gmail.com>
2023-05-23 19:54:22 +0900

    fix Head component Script

--- components/Head.js
+++ components/Head.js
@@ -1,5 +1,5 @@
 import Head from "next/head"
-
+import Script from 'next/script'
 
 const Head_site = () => {
     return (
@@ -8,25 +8,23 @@ const Head_site = () => {
                 <title>Hololive同時接続者数 リアルタイム シンプルUI</title>
                 <meta name="description" content="ホロライブの同時接続者数ランキングをチェックする。シンプルなUIで、勢いのあるライブが一目で分かる。" />
                 <link rel="icon" href="/webicon.jpg" />
-                {/* Google Analytics */}
-                <script
-                    async
-                    src="https://www.googletagmanager.com/gtag/js?id=G-G6BYLQRKYW"
-                />
-                <script
-                    dangerouslySetInnerHTML={{
-                    __html: `
+            </Head>
+
+            {/* Google Analytics */}
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-G6BYLQRKYW"
+                strategy="afterInteractive"
+            />
+            <Script strategy="afterInteractive">
+                {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', 'G-G6BYLQRKYW');
-                `,
-                }}
-                />
-            </Head>
+                `}
+            </Script>
         </div>
     )
 }
-
 
 export default Head_site
